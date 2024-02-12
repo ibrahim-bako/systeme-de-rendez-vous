2a323b1e4530612bc94dd02b99c94b5a9edac3d4
latifa <latifabadolo@gmail.com>
2024-02-12 19:11:44 +0100

    change database postgres to sqlite

--- app/settings.py
+++ app/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgesql',
-        'NAME': os.environ["POSTGRES_NAME"],
-        'USER': os.environ["POSTGRES_USER"],
-        'PASSWORD': os.environ["POSTGRES_PASSWORD"],
-        'HOST': os.environ["POSTGRES_HOST"],
-        'PORT': os.environ["POSTGRES_PORT"],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -96,7 +92,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+    }, 
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
